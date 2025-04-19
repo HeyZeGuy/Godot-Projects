6f66a9797ea5848538dbe76422ebfb5bef98e001
GuyShalom <guyshalom248@gmail.com>
2025-04-19 20:38:34 +0300

    Basic killzone

---
+++ 2d_platformer_test/scripts/Killzone.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class Killzone : Area2D
+{
+	private Timer timer;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		timer = GetNode<Timer>("Timer");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	private void _on_body_entered(Node2D body)
+	{
+		timer.Start();
+	}
+
+	private void _on_timer_timeout()
+	{
+		GetTree().ReloadCurrentScene();
+	}
+}
